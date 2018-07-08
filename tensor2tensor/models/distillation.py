4c13e07699c4d1f6d6de3216b4a165374c82a12e
Chia Yew Ken <chiayewken@gmail.com>
2018-07-08 23:08:12 +0800

    Added **kwargs to models.distallation

--- tensor2tensor/models/distillation.py
+++ tensor2tensor/models/distillation.py
@@ -46,7 +46,8 @@ class Distillation(t2t_model.T2TModel):
                mode=tf.estimator.ModeKeys.TRAIN,
                problem_hparams=None,
                data_parallelism=None,
-               decode_hparams=None):
+               decode_hparams=None,
+               **kwargs):
     assert hparams.distill_phase in ["train", "distill"]
 
     if hparams.distill_phase == "train" and hparams.teacher_learning_rate:
