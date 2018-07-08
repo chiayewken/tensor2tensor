9ad2102671b4bf4b818f3a73ad822d4e695ea42e
Chia Yew Ken <chiayewken@gmail.com>
2018-07-09 00:44:00 +0800

    *args and **kwargs

--- tensor2tensor/models/distillation.py
+++ tensor2tensor/models/distillation.py
@@ -46,7 +46,9 @@ class Distillation(t2t_model.T2TModel):
                mode=tf.estimator.ModeKeys.TRAIN,
                problem_hparams=None,
                data_parallelism=None,
-               decode_hparams=None):
+               decode_hparams=None,
+               *args,
+               **kwargs):
     assert hparams.distill_phase in ["train", "distill"]
 
     if hparams.distill_phase == "train" and hparams.teacher_learning_rate:
